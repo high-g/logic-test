a9928044b86308e9cfda2204e869a623fabe2f08
1−10 谷 <tani@1-10.com>
2018-01-02 23:24:27 +0900

    [Update]mysql simple get api

--- 007.connectivity/server/get.php
+++ 007.connectivity/server/get.php
@@ -1,4 +1,29 @@
 <?php
-$sql = "SELECT * FROM member";
+$host = 'localhost';
+$user = 'root';
+$password = 'root';
+$db = 'test_db';
+$port = 3306;
 
-echo($sql);
+// DB接続
+$mysqli = new mysqli($host, $user, $password, $db);
+if ($mysqli->connect_error) {
+  echo $mysqli->connect_error;
+  exit();
+} else {
+  $mysqli->set_charset("utf8");
+}
+
+// ここにDB処理いろいろ書く（後述）
+$sql = "SELECT * FROM member";
+$return_array = [];
+if($result = $mysqli->query($sql)){
+  while($row = $result->fetch_assoc()) {
+    $return_array[] = $row;
+  }
+}
+
+echo json_encode($return_array);
+
+// DB接続を閉じる
+$mysqli->close();
