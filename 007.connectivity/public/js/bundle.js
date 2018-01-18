fd8e61a10ab11cd4be6c5a2d8edd41205c368885
1−10 谷 <tani@1-10.com>
2018-01-18 19:47:49 +0900

    [Update]

--- 007.connectivity/public/js/bundle.js
+++ 007.connectivity/public/js/bundle.js
@@ -111,23 +111,6 @@ var table_member = document.getElementById('table_member');
 
 show();
 
-// get.then(responce => {
-//   for(let i=0; i<responce.length; i++) {
-// 
-//   }
-// })
-
-
-// str_table = '<tr>'
-//           + `<td>${responce[i].id}</td>`
-//           + `<td>${responce[i].name}</td>`
-//           + '</tr>';
-// 
-// div.innerHTML = str_table;
-// console.log(responce[i]);
-// console.log(str_table)
-// //table_member.appendChild(str_table);
-
 /***/ }),
 /* 1 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
--- 007.connectivity/src/babel/entry.js
+++ 007.connectivity/src/babel/entry.js
@@ -12,19 +12,3 @@ async function show() {
 
 show()
 
-// get.then(responce => {
-//   for(let i=0; i<responce.length; i++) {
-// 
-//   }
-// })
-
-
-// str_table = '<tr>'
-//           + `<td>${responce[i].id}</td>`
-//           + `<td>${responce[i].name}</td>`
-//           + '</tr>';
-// 
-// div.innerHTML = str_table;
-// console.log(responce[i]);
-// console.log(str_table)
-// //table_member.appendChild(str_table);
