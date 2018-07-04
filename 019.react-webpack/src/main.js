d4a2c16b0fd87b431141f15139261139e8906b8c
1−10 谷 <tani@1-10.com>
2018-07-04 11:21:39 +0900

    [update]

--- 019.react-webpack/src/main.js
+++ 019.react-webpack/src/main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './scss/index.scss'
 
 class App extends React.Component {
     render() {
--- 019.react-webpack/webpack.config.js
+++ 019.react-webpack/webpack.config.js
@@ -1,6 +1,16 @@
+const MODE = 'development';
+const enabledSourceMap = (MODE === 'development');
+
 module.exports = {
-  mode: 'development',
-  entry: './src/main.js',
+  mode: MODE,
+  devServer: {
+    contentBase: './dist',
+    inline: true,
+    hot: true,
+    open: true,
+    watchContentBase: true
+  },
+  entry: ['babel-polyfill', './src/main.js'],
   output: {
     path: `${__dirname}/dist/js`,
     filename: 'main.js'
@@ -17,6 +27,26 @@ module.exports = {
                 ['env', {'module': false}],
                 'react'
               ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: enabledSourceMap
             }
           }
         ]
