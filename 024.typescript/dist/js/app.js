ac4d50759a00dbbbdf0aab6b5aa8609abc204a89
1−10 谷 <tani@1-10.com>
2018-07-31 13:26:45 +0900

    [update]typescript型だけ

--- 024.typescript/dist/js/app.js
+++ 024.typescript/dist/js/app.js
@@ -9,3 +9,14 @@ var module1;
 })(module1 || (module1 = {}));
 var foo = new module1.foo();
 console.log(foo);
+var a = false;
+var b = 1;
+var c = 'hello';
+var d;
+// 型推論
+var e = 123;
+// 数値any
+var f;
+f = 111;
+var array1 = [1, 2, 3];
+console.log(a, b, c, d, e, f);
--- 024.typescript/src/ts/app.ts
+++ 024.typescript/src/ts/app.ts
@@ -4,3 +4,21 @@ namespace module1 {
 
 var foo = new module1.foo();
 console.log(foo);
+
+var a: boolean = false;
+var b: number = 1;
+var c: string = 'hello';
+var d: any;
+
+// 型推論
+var e: any = 123;
+
+// 数値any
+var f: any;
+f = 111;
+
+var array1: number = [1, 2, 3];
+
+
+
+console.log(a, b, c, d, e, f);
