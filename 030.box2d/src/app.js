d6ab33740d27f8d447aa18e58f0ca45074a043ec
1−10 谷 <tani@1-10.com>
2018-08-30 12:24:16 +0900

    [update]webpack設定更新

--- 030.box2d/src/app.js
+++ 030.box2d/src/app.js
@@ -1,2 +1,9 @@
 import './style.scss'
 import * as Box2D from 'box2dweb-commonjs'
+
+const WORLDSCALE = 30;
+
+// const world = new b2World(
+// 	new b2Vec2(0, 9.8), // 重力 横0 縦9.8
+// 	true // 止まったオブジェクトを計算対象から外す
+// );
