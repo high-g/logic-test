3ca220409b98c389e245524d69ba5434fe4dab2a
1−10 谷 <tani@1-10.com>
2018-08-30 13:24:54 +0900

    [update]box2dテスト

--- 030.box2d/src/app.js
+++ 030.box2d/src/app.js
@@ -1,9 +1,56 @@
 import './style.scss'
 import * as Box2D from 'box2dweb-commonjs'
 
-const WORLDSCALE = 30;
 
-// const world = new b2World(
-// 	new b2Vec2(0, 9.8), // 重力 横0 縦9.8
-// 	true // 止まったオブジェクトを計算対象から外す
-// );
+const pixelRatio = devicePixelRatio
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+context.scale(1 / pixelRatio, 1 / pixelRatio)
+
+const WORLDSCALE = 30
+
+// 世界
+const world = new Box2D.b2World(
+	new Box2D.b2Vec2(0, 9.8), // 重力 横0 縦9.8
+	true // 止まったオブジェクトを計算対象から外す
+)
+
+// 入れ物定義
+const fixDef = new Box2D.b2FixtureDef
+fixDef.density = 1.0 // 密度
+fixDef.friction = 0.5 // 摩擦係数
+fixDef.restitution = 0.2 // 反発係数
+
+// 物体宣言 - baseライン
+const bodyDef = new Box2D.b2BodyDef
+bodyDef.type = Box2D.b2Body.b2_staticBody
+fixDef.shape = new Box2D.b2PolygonShape
+
+// 縦1pxのライン
+fixDef.shape.SetAsBox(600 / WORLDSCALE, 1 / WORLDSCALE) // width, height
+bodyDef.position.Set(0, 400 / WORLDSCALE)
+world.CreateBody(bodyDef).CreateFixture(fixDef)
+
+// ボール
+bodyDef.type = Box2D.b2Body.b2_dynamicBody // 動く物体
+fixDef.shape = new Box2D.b2CircleShape(30 / WORLDSCALE)
+bodyDef.position.x = 300 / WORLDSCALE
+bodyDef.position.y = 0 / WORLDSCALE
+world.CreateBody(bodyDef).CreateFixture(fixDef)
+
+// デバッグ表示
+const debugDraw = new Box2D.b2DebugDraw()
+debugDraw.SetSprite(context)
+debugDraw.SetDrawScale(WORLDSCALE)
+debugDraw.SetFillAlpha(0.5)
+debugDraw.SetFlags(Box2D.b2DebugDraw.e_shapeBit | Box2D.b2DebugDraw.e_joinBit)
+world.SetDebugDraw(debugDraw)
+
+const update = () => {
+	world.Step(1/ 60, 10, 10);
+	world.DrawDebugData()
+	world.ClearForces()
+	requestAnimationFrame(update)
+}
+
+update()
