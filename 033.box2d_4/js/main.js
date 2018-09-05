4b8f1043f116c2e27e12ce2ce7ccc82826ae5f63
1−10 谷 <tani@1-10.com>
2018-09-05 12:56:05 +0900

    [update]

--- 033.box2d_4/js/main.js
+++ 033.box2d_4/js/main.js
@@ -1,19 +1,6 @@
-var b2Vec2 = Box2D.Common.Math.b2Vec2,
-b2BodyDef = Box2D.Dynamics.b2BodyDef,
-b2Body = Box2D.Dynamics.b2Body,
-b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
-b2Fixture = Box2D.Dynamics.b2Fixture,
-b2World = Box2D.Dynamics.b2World,
-b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
-b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
-b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+phina.globalize()
 
-var world, fixDef, bodyDef
+var SCREEN_WIDTH = 800;
+var SCREEN_HEIGHT = 600;
 
-var canvasWidth = 800
-var canvasHeight = 600
-
-var canvas = document.getElementById('canvas')
-canvas.width = canvasWidth
-canvas.height = canvasHeight
-
+phina.define()
