46c5c0661f887e6f66faff7a0943f9f8663a94c2
1−10 谷 <tani@1-10.com>
2018-09-05 12:41:22 +0900

    [update]

--- 000.box2d_simple_template/js/main.js
+++ 000.box2d_simple_template/js/main.js
@@ -8,15 +8,36 @@ b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
 b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
 b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
 
-var canvasWidth = 800;
-var canvasHeight = 600;
+var init = function() {
+	world = new b2World(new b2Vec2(0, 1000), true)
 
-var canvas = document.getElementById('canvas');
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
+	// 剛体（物体）定義
+	bodyDef = new b2BodyDef
 
-function update() {
+	// 剛体のプロパティ定義
+	fixDef = new b2FixtureDef
+	fixDef.density = 1.0
+	fixDef.friction = 0.5
+	fixDef.restitution = 0.2
+
+
+
+	// setup debug draw
+	debugDraw = new b2DebugDraw()
+	debugDraw.SetSprite(canvas.getContext('2d'))
+	debugDraw.SetFillAlpha(0.3)
+	debugDraw.SetLineThickness(1.0)
+	debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit)
+	world.SetDebugDraw(debugDraw)
+	
+	update()
+};
+
+var update = function() {
+	world.Step(1/60, 10, 10)
+	world.DrawDebugData()
+	world.ClearForces()
 	requestAnimationFrame(update);
-}
+};
 
-update();
+init();
