9c6faa2d02afadeb2aae3d055d4d8f4b8c8f8e77
1−10 谷 <tani@1-10.com>
2018-09-05 12:46:15 +0900

    [update]phina+box2d初期状態

---
+++ 033.box2d_4/js/main.js
@@ -0,0 +1,19 @@
+var b2Vec2 = Box2D.Common.Math.b2Vec2,
+b2BodyDef = Box2D.Dynamics.b2BodyDef,
+b2Body = Box2D.Dynamics.b2Body,
+b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+b2Fixture = Box2D.Dynamics.b2Fixture,
+b2World = Box2D.Dynamics.b2World,
+b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+
+var world, fixDef, bodyDef
+
+var canvasWidth = 800
+var canvasHeight = 600
+
+var canvas = document.getElementById('canvas')
+canvas.width = canvasWidth
+canvas.height = canvasHeight
+
