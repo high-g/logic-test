6c0e59a2caf5ff4ac83a929b3b8caa3c1703a8bf
1−10 谷 <tani@1-10.com>
2018-10-11 13:36:33 +0900

    [update]vueコンポーネントテスト

---
+++ 033.vue_storybook/src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p>Hello, {{name}}</p>
+		<p>テスト</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			name: 'world'
+		}
+	}
+}
+</script>
+
+<style scoped>
+h1 {
+	color: darkcyan;
+}
+</style>
