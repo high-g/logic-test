467c676606a50bb0263de244432e3de27195992b
1−10 谷 <tani@1-10.com>
2018-10-12 16:17:54 +0900

    [update]Anchorコンポーネント

---
+++ 033.vue_storybook/src/components/Anchor.vue
@@ -0,0 +1,24 @@
+<template>
+	<span class="anchor" @click="onClick()">
+		<slot></slot>
+	</span>
+</template>
+
+<script>
+export default {
+	props: ['href'],
+	methods: {
+		onClick() {
+			location.href = this.href
+		}
+	}
+}
+</script>
+
+<style scoped>
+.anchor {
+	color: blue;
+	text-decoration: underline;
+	cursor: pointer;
+}
+</style>
--- 033.vue_storybook/src/index.js
+++ 033.vue_storybook/src/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 
 import Hello from './components/Hello.vue'
+import Anchor from './components/Anchor.vue'
 
 new Vue({
   el: '#app',
   components: {
-    hello: Hello
+    hello: Hello,
+    anchor: Anchor
   }
 })
