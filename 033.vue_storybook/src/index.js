6d8af6469d6b36c887682b22f5bbbcc565e2b57f
1−10 谷 <tani@1-10.com>
2018-10-16 14:04:40 +0900

    [update]props, 型定義,

--- 033.vue_storybook/src/index.js
+++ 033.vue_storybook/src/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 
 import Hello from './components/Hello.vue'
 import Anchor from './components/Anchor.vue'
+import AnchorLink from './components/AnchorLink.vue'
 
 new Vue({
   el: '#app',
   components: {
-    hello: Hello,
-    anchor: Anchor
+    Hello,
+    Anchor,
+    AnchorLink
   }
 })
