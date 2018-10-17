fd54990bc1d76e2770ee2c41a2c715735fe4bc90
1−10 谷 <tani@1-10.com>
2018-10-17 13:31:04 +0900

    [update]$emit

---
+++ 033.vue_storybook/src/components/EventButtons.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<button @click="emitEventOne">Event1</button>
+		<button @click="emitEventTwo">Event2</button>
+		<button @click="emitEventThree">Event3</button>
+	</div>
+</template>
+
+<script>
+export default {
+	methods: {
+		emitEventOne() {
+			this.$emit('event-one')
+		},
+		emitEventTwo() {
+			this.$emit('event-two', 'This is an argument')
+		},
+		emitEventThree() {
+			this.$emit('event-three', 123, {name: 'three'})
+		}
+	}
+}
+</script>
+
+<style>
+
+
+</style>
--- 033.vue_storybook/src/index.js
+++ 033.vue_storybook/src/index.js
@@ -3,12 +3,31 @@ import Vue from 'vue'
 import Hello from './components/Hello.vue'
 import Anchor from './components/Anchor.vue'
 import AnchorLink from './components/AnchorLink.vue'
+import EventButtons from './components/EventButtons.vue'
 
 new Vue({
   el: '#app',
+  data: {
+    arguments: []
+  },
   components: {
     Hello,
     Anchor,
-    AnchorLink
+    AnchorLink,
+    EventButtons
+  },
+  methods: {
+    onEventOne() {
+      alert('Event 1')
+    },
+    onEventTwo(argument) {
+      this.arguments = []
+      this.arguments.push(argument)
+    },
+    onEventThree(arg1, arg2) {
+      this.arguments = []
+      this.arguments.push(arg1)
+      this.arguments.push(arg2)
+    }
   }
 })
