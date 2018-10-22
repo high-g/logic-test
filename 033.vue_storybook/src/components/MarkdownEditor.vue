478333fc4e03fd65f085843115bde220787b0b69
1−10 谷 <tani@1-10.com>
2018-10-22 15:18:52 +0900

    [update]

--- 033.vue_storybook/src/components/MarkdownEditor.vue
+++ 033.vue_storybook/src/components/MarkdownEditor.vue
@@ -1,9 +1,19 @@
 <template>
-	
+	<div class="from-group">
+		<textarea
+			class="from-input"
+			:value="text"
+			@input="onInput"
+			rows="10"
+		></textarea>
+	</div>
 </template>
 
 <script>
 export default {
+	props: {
+		value: {type: String, required: true}
+	}
 
 }
 </script>
