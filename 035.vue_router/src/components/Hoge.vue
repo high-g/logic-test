adfca672a8e2f778b36a688469cf029cdfb06e43
1−10 谷 <tani@1-10.com>
2018-10-27 10:08:35 +0900

    [update]vue-routerテスト

---
+++ 035.vue_router/src/components/Hoge.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hoge',
+  data () {
+    return {
+      msg: 'test'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
