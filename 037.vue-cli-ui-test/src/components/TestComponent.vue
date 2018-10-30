62fc41aa65372814533e942bcee2070b43f05e91
1−10 谷 <tani@1-10.com>
2018-10-30 18:36:34 +0900

    [update]

---
+++ 037.vue-cli-ui-test/src/components/TestComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="yes" @click="callYes">Yes</button>
+    <button class="no" @click="callNo">No</button>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'YesNoComponent',
+  data() {
+    return {
+      text: '',
+    };
+  },
+  methods: {
+    callMe(text) {
+      this.text = text;
+    },
+    callYes() {
+      this.callMe('yes');
+    },
+    callNo() {
+      this.callMe('no');
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
+
