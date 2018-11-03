5c876dd4960aad10e9cb4ecec61200630926ac46
1−10 谷 <tani@1-10.com>
2018-11-03 23:50:43 +0900

    [update]bootstrap環境

--- 015.bootstrap/webpack.config.js
+++ 015.bootstrap/webpack.config.js
@@ -1,5 +1,8 @@
+const MODE = 'development'
+const enabledSourceMap = (MODE === 'development')
+
 module.exports = {
-    mode: 'production',
+    mode: MODE,
     module: {
         rules: [
             {
@@ -8,7 +11,34 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: true,
+                            sourceMap: enabledSourceMap,
+                            // 0 => no loaders (default);
+                            // 1 => postcss-loader;
+                            // 2 => postcss-loader, sass-loader
+                            importLoaders: 2
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: enabledSourceMap
+                        }
+                    }
+                ]
+            },
+            // {
+            //     test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+            //     loader: 'url-loader'
+            // }
         ]
     }
 };
