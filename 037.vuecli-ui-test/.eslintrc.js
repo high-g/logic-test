9deba6ee94e7b935cbe29e1d9a706b8e14e14f26
1−10 谷 <tani@1-10.com>
2018-11-14 11:46:34 +0900

    [update]lintを通すために

--- 037.vuecli-ui-test/.eslintrc.js
+++ 037.vuecli-ui-test/.eslintrc.js
@@ -1,26 +1,17 @@
 module.exports = {
   root: true,
-
   env: {
     node: true,
   },
-
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
-
   rules: {
     'no-console': 'off',
-    'no-debugger': 'off'
+    'no-debugger': 'off',
   },
-
   parserOptions: {
     parser: 'babel-eslint',
   },
-
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/airbnb'
-  ]
 };
--- 037.vuecli-ui-test/tests/unit/component.spec.js
+++ 037.vuecli-ui-test/tests/unit/component.spec.js
@@ -1,33 +1,27 @@
-import { shallowMount, mount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
-import YesNoComponent from '@/components/YesNoComponent.vue';
+// import { shallowMount, mount } from '@vue/test-utils';
 
-describe('HelloWorld.vue testしていくよ', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+// describe('HelloWorld.vue testしていくよ', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message';
+//     // const wrapper = shallowMount(HelloWorld, {
+//     //   propsData: { msg },
+//     // });
+//     // expect(wrapper.text()).toMatch(msg);
+//   });
 
-  // test('equal', () => {
-  //   expect(0.1 + 0.2).toBeCloseTo(0.35, 0)
-  //   expect(0.1 + 0.2).toBeCloseTo(0.35, 1)
-  //   expect(0.1 + 0.2).not.toBeCloseTo(0.35, 2)
-  //   expect(0.1 + 0.2).toBeCloseTo(0.3, 1)
-  // })
+//   // test('equal', () => {
+//   //   expect(0.1 + 0.2).toBeCloseTo(0.35, 0)
+//   //   expect(0.1 + 0.2).toBeCloseTo(0.35, 1)
+//   //   expect(0.1 + 0.2).not.toBeCloseTo(0.35, 2)
+//   //   expect(0.1 + 0.2).toBeCloseTo(0.3, 1)
+//   // })
 
-  // test('string length', () => {
-  //   expect('abcdef').toHaveLength(6)
-  // })
+//   // test('string length', () => {
+//   //   expect('abcdef').toHaveLength(6)
+//   // })
 
-  // test('型チェック length', () => {
-  //   expect(() => {}).toBeInstanceOf(Function)
-  // })
-});
-
-describe('YesNoComponent.vue test', () => {
-  
-})
+//   // test('型チェック length', () => {
+//   //   expect(() => {}).toBeInstanceOf(Function)
+//   // })
+// });
 
