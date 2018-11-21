6fd0013831a53b972342af1703185d3dfb0488f6
1−10 谷 <tani@1-10.com>
2018-11-21 11:49:14 +0900

    [update]

--- 037.vuecli-ui-test/src/components/TestComponent.vue
+++ 037.vuecli-ui-test/src/components/TestComponent.vue
@@ -1,14 +1,27 @@
 <template>
 	<div class="blog-post">
 		<h3>aaaaa</h3>
+    <p>cnt: {{ cnt }}</p>
+    <button @click="increment">＋</button>
+    <button @click="decrement">ー</button>
 	</div>
 </template>
 
 <script>
 export default {
 	data () {
-		return {}
-	}
+		return {
+      cnt: 0
+    }
+	},
+  methods: {
+    increment() {
+      this.cnt++
+    },
+    decrement() {
+      this.cnt--
+    }
+  }
 }
 </script>
 
