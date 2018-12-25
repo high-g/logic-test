a3b51b91a05dd3dd17ab55598320e939da859aa5
1−10 谷 <tani@1-10.com>
2018-12-25 20:01:15 +0900

    [update]prettierテスト

---
+++ 039.prettier/src/index.js
@@ -0,0 +1,21 @@
+function hoge() {
+  console.log(1);
+  console.log(2);
+
+  console.log('hoge');
+
+  console.log(1 + 'pp' + 'px');
+}
+
+hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge() ||
+  hoge();
