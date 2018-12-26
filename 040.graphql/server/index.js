a73244192b5faf821f375a8ee3de028811a04187
1−10 谷 <tani@1-10.com>
2018-12-26 18:44:59 +0900

    [update]

--- 040.graphql/server/index.js
+++ 040.graphql/server/index.js
@@ -1,3 +1,15 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
+
+// mock
+const books = [
+  {
+    title: 'Harry Potter',
+    author: 'J.K. Rowlling',
+    price: 2000
+  },
+  {}
+]
