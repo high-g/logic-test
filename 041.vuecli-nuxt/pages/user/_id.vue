5ea05b8125eac43889ebb5d83b19920b2f531ab7
1−10 谷 <tani@1-10.com>
2019-01-10 18:40:40 +0900

    [update]

---
+++ 041.vuecli-nuxt/pages/user/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="container">
+    <p>id: {{ getId }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  computed: {
+    getId() {
+      console.log(this.$router)
+      return this.$router.params
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
+
