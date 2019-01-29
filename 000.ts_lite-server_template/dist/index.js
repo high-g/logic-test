30c4ecc83afa851b6a4d01f2d5f441641a0bcff7
1−10 谷 <tani@1-10.com>
2019-01-29 17:10:48 +0900

    [update]ts最小動作環境

---
+++ 000.ts_lite-server_template/dist/index.js
@@ -0,0 +1,7 @@
+// Hello World
+var message = 'Hello! TypeScript!';
+console.log(message);
+var x = true;
+console.log('x', x);
+console.log('aaaa');
+console.log(12);
---
+++ 000.ts_lite-server_template/src/index.ts
@@ -0,0 +1,10 @@
+// Hello World
+const message:string = 'Hello! TypeScript!'
+console.log(message)
+
+const x: boolean = true
+console.log('x',x)
+
+console.log('aaaa')
+
+console.log(12)
