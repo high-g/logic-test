78d503702c0e01a7866f6787680f775d0f2c4bf1
1−10 谷 <tani@1-10.com>
2019-01-29 18:08:37 +0900

    [update]type script

--- 045.typescript-helloworld/dist/index.js
+++ 045.typescript-helloworld/dist/index.js
@@ -1,7 +1,65 @@
 // Hello World
 var message = 'Hello! TypeScript!';
 console.log(message);
-var x = true;
-console.log('x', x);
-console.log('aaaa');
-console.log(12);
+// primitive
+var a = true;
+var b = 1;
+var c = 'hello';
+var d = null;
+console.log(a, b, c, d);
+// array
+var x = [1, 2, 3];
+var y = ['a', 'b', 'c'];
+var z = [null, 1, 'b'];
+console.log(x, y, z);
+// enum(列挙型)
+var Color;
+(function (Color) {
+    Color[Color["Blue"] = 0] = "Blue";
+    Color[Color["Red"] = 1] = "Red";
+})(Color || (Color = {}));
+var enum_a = Color.Blue;
+console.log(enum_a);
+console.log(enum_a === Color.Blue);
+console.log(enum_a === 0);
+var BBOY;
+(function (BBOY) {
+    BBOY[BBOY["Taisuke"] = 10000] = "Taisuke";
+    BBOY[BBOY["Ryoma"] = 5000] = "Ryoma";
+})(BBOY || (BBOY = {}));
+console.log(BBOY.Taisuke);
+// normal function
+function f1() {
+    console.log('message');
+}
+function f2(x) {
+    return x.toLocaleString() + '円';
+}
+function f3(x, y) {
+    if (y === void 0) { y = '円'; }
+    return x.toLocaleString() + y;
+}
+f1();
+console.log(f2(100));
+console.log(f3(1, 'ペソ'));
+// allow function
+var add = function (x) { return x + 1; };
+var add2 = function (x) {
+    return x + 1;
+};
+console.log('add(1)', add(1));
+console.log('add2(2)', add(2));
+var A = /** @class */ (function () {
+    function A() {
+        this.x = 1;
+        this.y = 'a';
+    }
+    return A;
+}());
+var classA = new A();
+console.log('classA', classA);
+var aaa = 1;
+{
+    var aaa_1 = 2;
+}
+console.log('aaa', aaa);
--- 045.typescript-helloworld/src/index.ts
+++ 045.typescript-helloworld/src/index.ts
@@ -1,10 +1,82 @@
 // Hello World
 const message:string = 'Hello! TypeScript!'
+
 console.log(message)
 
-const x: boolean = true
-console.log('x',x)
 
-console.log('aaaa')
+// primitive
+const a: boolean = true
+const b: number = 1
+const c: string = 'hello'
+const d: any = null
 
-console.log(12)
+console.log(a, b, c, d)
+
+
+// array
+const x: number[] = [1, 2, 3]
+const y: string[] = ['a', 'b', 'c']
+const z: any[] = [null, 1 , 'b']
+
+console.log(x, y, z)
+
+
+// enum(列挙型)
+enum Color {Blue, Red}
+const enum_a: Color = Color.Blue
+
+console.log(enum_a)
+console.log(enum_a === Color.Blue)
+console.log(enum_a === 0)
+
+enum BBOY {Taisuke=10000,Ryoma=5000}
+console.log(BBOY.Taisuke)
+
+
+// normal function
+function f1(): void {
+  console.log('message')
+}
+
+function f2(x: number): string {
+  return x.toLocaleString() + '円'
+}
+
+function f3(x: number, y:string = '円'): string {
+  return x.toLocaleString() + y
+}
+
+f1()
+console.log(f2(100))
+console.log(f3(1, 'ペソ'))
+
+
+// allow function
+const add = (x: number): number => x+1;
+
+const add2 = (x: number): number => {
+  return x+1;
+}
+
+console.log('add(1)', add(1))
+console.log('add2(2)', add(2))
+
+class A {
+  private x: number = 1
+  public y: string = 'a'
+
+  constructor() {
+    
+  }
+}
+
+const classA: A = new A()
+
+console.log('classA', classA)
+
+
+let aaa = 1
+{
+  let aaa = 2
+}
+console.log('aaa',aaa)
