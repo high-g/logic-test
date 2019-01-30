3b85e92aee18076e4614665c5588defd22aceb68
1−10 谷 <tani@1-10.com>
2019-01-30 22:36:14 +0900

    [update]event

--- 045.react-normal/src/components/HelloFunctional.js
+++ 045.react-normal/src/components/HelloFunctional.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+const name = 'testだよ'
+
 const HelloFunctional = (props) => (
   <div>
     <h1>HelloFunctional</h1>
+    <div>{name}</div>
     <div>{props.data}</div>
   </div>
 )
--- 045.react-normal/src/components/HelloWorld.js
+++ 045.react-normal/src/components/HelloWorld.js
@@ -8,7 +8,8 @@ class HelloWorld extends Component {
     this.state = {
       flg: false,
       value1: 'foo',
-      value2: ['bar', 'baz']
+      value2: ['bar', 'baz'],
+      name: 'checkだよ'
     }
     this.handleClick = this.handleClick.bind(this)
     this.flgChange = this.flgChange.bind(this)
@@ -18,19 +19,35 @@ class HelloWorld extends Component {
     console.log(this.state.value1)
   }
 
+  handleChange(e) {
+    const title = e.target.value
+    this.setState({value1: title})
+  }
+
   flgChange() {
     this.setState({flg: !this.state.flg})
     this.setState({value1: this.state.flg ? 'foo' : 'bar'})
   }
 
+  getNum(num) {
+    return 1 + num;
+  }
+
   render() {
+    setTimeout(() => {
+      this.setState({name: 'ゴリラだよ'})
+    }, 1000);
+
     return (
       <div>
         <h1>HelloWorld</h1>
         <div onClick={this.handleClick}>let's click</div>
         <div onClick={this.flgChange}>let's click2</div>
+        <b>{this.getNum(2)}</b>
+        <p>{this.state.name}</p>
         <HelloFunctional data={this.state.value1} />
         <HelloFunctional data={String(this.state.flg)} />
+        <input value={this.state.value1} onChange={this.handleChange.bind(this)} />
       </div>
     );
   }
