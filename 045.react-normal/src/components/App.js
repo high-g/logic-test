5434dd26a9e0397673dda38dd55bd77faf8e3d09
1−10 谷 <tani@1-10.com>
2019-01-30 14:00:33 +0900

    [update]HelloWorld

--- 045.react-normal/src/components/App.js
+++ 045.react-normal/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <HelloWorld />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
