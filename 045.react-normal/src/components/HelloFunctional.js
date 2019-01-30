3c1b0f678b1e03e17b52cb0926f227841e774786
1−10 谷 <tani@1-10.com>
2019-01-30 22:08:38 +0900

    [update]functional, state, props...

---
+++ 045.react-normal/src/components/HelloFunctional.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HelloFunctional = (props) => (
+  <div>
+    <h1>HelloFunctional</h1>
+    <div>{props.data}</div>
+  </div>
+)
+
+export default HelloFunctional
--- 045.react-normal/src/components/HelloWorld.js
+++ 045.react-normal/src/components/HelloWorld.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import HelloFunctional from './HelloFunctional'
 
 class HelloWorld extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      flg: false,
+      value1: 'foo',
+      value2: ['bar', 'baz']
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.flgChange = this.flgChange.bind(this)
+  }
+
+  handleClick() {
+    console.log(this.state.value1)
+  }
+
+  flgChange() {
+    this.setState({flg: !this.state.flg})
+    this.setState({value1: this.state.flg ? 'foo' : 'bar'})
+  }
+
   render() {
     return (
-      <h1>HelloWorld</h1>
+      <div>
+        <h1>HelloWorld</h1>
+        <div onClick={this.handleClick}>let's click</div>
+        <div onClick={this.flgChange}>let's click2</div>
+        <HelloFunctional data={this.state.value1} />
+        <HelloFunctional data={String(this.state.flg)} />
+      </div>
     );
   }
 }
