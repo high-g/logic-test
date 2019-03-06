12a8a155f4016d291faef806894761e50a7e7c76
1−10tani <tani@1-10.com>
2019-03-06 17:30:50 +0900

    [update]serviceworker

--- 038.workbox/dist/sw.js
+++
@@ -1,6 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js')
-if(workbox) {
-  console.log('Yeah! workbox did load')
-} else {
-  console.log('Oh, workbox did not load')
-}
