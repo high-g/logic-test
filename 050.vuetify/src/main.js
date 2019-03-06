883a42d4c98ac8a80955b6ea782f597756007c76
1−10tani <tani@1-10.com>
2019-03-06 22:05:53 +0900

    [update]

--- 050.vuetify/src/main.js
+++ 050.vuetify/src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
-Vue.use(Vuetify)
 
 new Vue({
   router,
