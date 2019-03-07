9aa1a82c2f46e5ad51d113d0d07dff141b69bf9f
1−10tani <tani@1-10.com>
2019-03-07 14:10:23 +0900

    [update]

---
+++ 050.vuetify/src/views/Grid.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-flex xs3 md9>
+    <v-card>
+      <v-card-title>新規会員登録フォーム</v-card-title>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
