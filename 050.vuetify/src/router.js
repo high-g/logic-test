9e2dbe721884af967fdacdc607ed20817f51556a
1−10tani <tani@1-10.com>
2019-03-08 14:09:17 +0900

    [update]vuetify

--- 050.vuetify/src/router.js
+++ 050.vuetify/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Grid from './views/Grid.vue'
 import Grid2 from './views/Grid2.vue'
+import Test1 from './views/Test1.vue'
 
 Vue.use(Router)
 
@@ -21,6 +22,10 @@ export default new Router({
     {
       path: '/grid2',
       component: Grid2
+    },
+    {
+      path: '/test1',
+      component: Test1
     }
   ]
 })
---
+++ 050.vuetify/src/views/Test1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-toolbar class="blue darken-1">
+      <v-toolbar-side-icon @click="menu"></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-container :class="{'d-none': !viewMenu}">
+      aaa
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      viewMenu: false
+    }
+  },
+  methods: {
+    menu() {
+      this.viewMenu = !this.viewMenu
+    }
+  }
+}
+</script>
