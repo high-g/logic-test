e2f2c223ecdd336fe87b5d621b6318d7e28d5355
tani <tani@1-10.com>
2019-12-10 13:14:53 +0900

    [update]nuxt-link, transition css

---
+++ 067.nuxt-learn/pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>Hi from about</p>
+    <nuxt-link to="/">top</nuxt-link>
+  </div>
+</template>
+
--- 067.nuxt-learn/pages/books.vue
+++ 067.nuxt-learn/pages/books.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <nuxt-child />
-  </div>
+  <transition><nuxt-child /></transition>
 </template>
