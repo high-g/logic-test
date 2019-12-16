08e2d47ba0ddcb41e522c8f9c85799f2fda8e9ac
tani <tani@1-10.com>
2019-12-16 18:45:50 +0900

    [update]nuxtファイル読み書き

---
+++ 069.nuxt-fileinout/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <input type="text" v-model="name">
+    <button @click="setBlobUrl">add</button>
+    <ul>
+      <li v-for="item in member" :key="item.key">
+        {{ item.title }} | {{ item.explain }} | {{ item.created }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: '',
+        member: []
+      }
+    },
+    async asyncData({ app }) {
+      const store = await app.$axios.$get('/store.json') || []
+      return {member: store}
+    },
+    methods: {
+      setBlobUrl() {
+        console.log('name',this.name)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
