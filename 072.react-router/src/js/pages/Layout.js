e44211229875fbf53fd9e9c8b357b95095783baa
tani <tani@1-10.com>
2020-04-09 15:32:21 +0900

    [update]react-router

---
+++ 072.react-router/src/js/pages/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <h1>KillerNews.net</h1>
+    );
+  }
+}
+ 
+export default Layout;
